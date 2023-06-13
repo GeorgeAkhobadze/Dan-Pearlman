9d9e98a25db30248ad035e282b87b410eed87bda
axobadzegiorgi5 <axobadzegiorgi5@gmail.com>
2023-06-13 14:17:12 +0400

    rest of the pages

--- burgerMenu.js
+++ burgerMenu.js
@@ -4,9 +4,11 @@ const burgerClose = document.getElementById('burger-close-btn');
 console.log(burgerBtn);
 
 burgerBtn.addEventListener('click', function () {
-  burgerMenu.style.transform = 'opacity: 1';
+  burgerMenu.style.opacity = '1';
+  burgerMenu.style.pointerEvents = 'auto';
 });
 
 burgerClose.addEventListener('click', function () {
-  burgerMenu.style.transform = 'opacity: 0';
+  burgerMenu.style.opacity = '0';
+  burgerMenu.style.pointerEvents = 'none';
 });
